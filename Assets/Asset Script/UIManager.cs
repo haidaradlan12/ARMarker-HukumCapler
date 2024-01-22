56622d5a21d25dc1d5ef48c3a7146e2634108a44
Haidaradlan12 <adlannufahaidar12@gmail.com>
2024-01-22 15:53:00 +0700

    Initial commit

---
+++ Assets/Asset Script/UIManager.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+
+public class UIManager : MonoBehaviour
+{
+    public RectTransform MenuAtas, MenuBawah, MenuKiri, Button, Informasi, Petunjuk;
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        MenuAtas.DOAnchorPos(new Vector2(0, 0), 1.0f);
+        MenuBawah.DOAnchorPos(new Vector2(0, 0), 1.0f);
+        MenuKiri.DOAnchorPos(new Vector2(-2000, 0), 1.0f);
+        Button.DOAnchorPos(new Vector2(0, 0), 1.0f);
+        Informasi.DOAnchorPos(new Vector2(2000, 0), 1.0f);
+        Petunjuk.DOAnchorPos(new Vector2(2000, 0), 1.0f);
+    }
+    public void ButtonKembali()
+    {
+        MenuAtas.DOAnchorPos(new Vector2(0, 0), 1.0f);
+        MenuBawah.DOAnchorPos(new Vector2(0, 0), 1.0f);
+        MenuKiri.DOAnchorPos(new Vector2(-2000, 0), 1.0f);
+        Button.DOAnchorPos(new Vector2(0, 0), 1.0f);
+        Informasi.DOAnchorPos(new Vector2(2000, 0), 1.0f);
+        Petunjuk.DOAnchorPos(new Vector2(2000, 0), 1.0f);
+    }
+    public void ButtonInformasi()
+    {
+        MenuAtas.DOAnchorPos(new Vector2(0, 2500), 1.0f);
+        MenuBawah.DOAnchorPos(new Vector2(0, -2500), 1.0f);
+        MenuKiri.DOAnchorPos(new Vector2(0, 0), 1.0f);
+        Button.DOAnchorPos(new Vector2(2000, 0), 1.0f);
+        Informasi.DOAnchorPos(new Vector2(0, 0), 1.0f);
+        Petunjuk.DOAnchorPos(new Vector2(2000, 0), 1.0f);
+    }
+    public void ButtonGuide()
+    {
+        MenuAtas.DOAnchorPos(new Vector2(0, 2500), 1.0f);
+        MenuBawah.DOAnchorPos(new Vector2(0, -2500), 1.0f);
+        MenuKiri.DOAnchorPos(new Vector2(0, 0), 1.0f);
+        Button.DOAnchorPos(new Vector2(2000, 0), 1.0f);
+        Informasi.DOAnchorPos(new Vector2(2000, 0), 1.0f);
+        Petunjuk.DOAnchorPos(new Vector2(0, 0), 1.0f);
+    }
+    
+}
---
+++ Assets/Asset Script/UIManagerMain.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+
+public class UIManagerMain : MonoBehaviour
+{
+    public GameObject Object1, Object2;
+    Animator Animator1, Animator2;
+    float prevSpeed1, prevSpeed2, locprevspeed1, locprevspeed2;
+    private void Update()
+    {
+        locprevspeed1 = prevSpeed1;
+        locprevspeed2 = prevSpeed2;
+    }
+    public void AnimasiPause()
+    {
+        Animator1 = Object1.GetComponent<Animator>();
+        Animator2 = Object2.GetComponent<Animator>();
+        prevSpeed1 = Animator1.speed;
+        prevSpeed2 = Animator2.speed;
+        Animator1.speed = 0;
+        Animator2.speed = 0;
+    }
+
+    public void AnimasiPlay()
+    {
+        Animator1 = Object1.GetComponent<Animator>();
+        Animator2 = Object2.GetComponent<Animator>();
+        Animator1.speed = locprevspeed1;
+        Animator2.speed = locprevspeed2;
+    }
+}
